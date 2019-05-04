bd94ee0e2a859d16c762100b7b827e30edf86597
Heath Banner <God@Heaths-MBP.lan1>
2019-05-04 14:12:37 -0400

    Debug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var routes = require('./htmlRoutes');
 var api = require('./apiRoutes.js');
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
